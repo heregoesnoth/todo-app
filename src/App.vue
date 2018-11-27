6f63639a63590cc95421f52ece3413bcbb1f7490
Lucas Gomes <lucas@polisys.com.br>
2018-11-27 01:57:28 -0200

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,59 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <img alt="Vue logo" src="./assets/logo.png">
+        <todo-list v-bind:todos="todos"></todo-list>
+        <create-todo v-on:create-todo="createTodo"></create-todo>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import sweetalert from 'sweetalert';
+    import TodoList from './components/TodoList.vue'
+    import CreateTodo from './components/CreateTodo.vue'
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'app',
+        components: {
+            TodoList,
+            CreateTodo
+        },
+        data() {
+            return {
+                todos: [{
+                    title: 'Todo A',
+                    project: 'Project A',
+                    done: false
+                }, {
+                    title: 'Todo B',
+                    project: 'Project B',
+                    done: true
+                }, {
+                    title: 'Todo C',
+                    project: 'Project C',
+                    done: false,
+                }, {
+                    title: 'Todo D',
+                    project: 'Project D',
+                    done: false,
+                }]
+            }
+        },
+        methods: {
+            createTodo(newTodo) {
+                this.todos.push(newTodo);
+                sweetalert('Success!', 'To-Do created!', 'success');
+            },
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
 </style>
